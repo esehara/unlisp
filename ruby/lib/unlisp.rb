097f70b82302db0bba59217a0bfe2293ae2fe935
esehara shigeo <esehara@gmail.com>
2016-05-25 03:46:03 +0900

    Add Machine Class for play pry ;)

--- ruby/lib/unlisp.rb
+++ ruby/lib/unlisp.rb
@@ -4,4 +4,10 @@ require "unlisp/token"
 require "unlisp/parser"
 
 module Unlisp
+  class Machine
+    def toplevel_eval l
+      lst = Unlisp::Lexer::list_analyzer l
+      psr = Unlisp::Parser::list_eval lst
+    end
+  end
 end
