d0f82018e57f404576554a0fe1d1b7f1aad3cd0a
esehara shigeo <esehara@gmail.com>
2016-05-25 03:47:47 +0900

    Use reduce

--- ruby/lib/unlisp/parser.rb
+++ ruby/lib/unlisp/parser.rb
@@ -2,27 +2,34 @@ require 'unlisp/lexer'
 
 module Unlisp
   module Parser
+
+    def apply lst
+      lst.map do |x|
+        if x.list?
+          x = list_eval x.value
+        else
+          x
+        end
+      end
+    end
+
     def list_eval lst
       if lst[0].type == Unlisp::Token::ATOM
         case lst[0].value
         when "+"
           Token.new(Unlisp::Token::INTEGER, plus(lst))
+        when "print"
+          lst.shift
+          lst = apply lst
+          puts lst.value
         end
       end
     end
 
     def plus lst
       lst.shift
-      lst.reduce do |x, y|
-        x, y = [x, y].map do |z|
-          if z.type == Unlisp::Token::LIST
-            z = list_eval z.value
-          else
-            z
-          end
-        end
-        x.value + y.value
-      end
+      lst = apply lst
+      lst.reduce {|x, y| x.value + y.value}
     end
   end
 
